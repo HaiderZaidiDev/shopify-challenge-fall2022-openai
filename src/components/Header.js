bc942c2dfd061baf000ecec692071f8990c05a61
Haider Zaidi <contact@haiderzaidi.ca>
2022-05-22 19:19:20 -0400

    Added repo link to header, added hover to buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
             <div className="header-content">
                 <h1>Exhibiting the Power of GPT-3 </h1>
                 <h2> Created for the Shopify Front End Developer Challenge (Fall 2022), by Haider Zaidi </h2>
-                <button className="cta-btn"> View Repository </button>
+                <a href="https://github.com/HaiderZaidiDev/shopify-challenge-fall2022-openai" target="_blank"> <button className="cta-btn"> View Repository </button></a>
             </div>
         </Row>
       </Col>
